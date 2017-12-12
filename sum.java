81f27e8e6def62e699d46196b21275abe3d329e5
root <root@localhost.localdomain>
2017-12-12 14:15:22 -0800

    sum of three values program

---
+++ sum.java
@@ -0,0 +1,13 @@
+class sum.java
+{
+	public static void main (String [] args)
+	{
+		int a,b,c;
+		a=50;
+		b=30;
+		c=120;
+		c=a+b+c;
+		System.out.println("The sum of three values are "+c);
+	}
+}
+
